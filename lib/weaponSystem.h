d8dc55d35d640d31cd8b1042b8c56adf2bc5800f
quy109372 <phanphuquy12.bh@gmail.com>
2026-05-29 10:44:48 +0700

    patch bullet type name

--- lib/weaponSystem.h
+++ lib/weaponSystem.h
@@ -1,14 +1,14 @@
 #pragma once
 #include <string>
 #include <vector>
-inline std::vector<std::string> weaponType = {"basicBulletType","basicBulletType","laserBulletType","lightningBulletType", "missileBulletType"};
+inline std::vector<std::string> weaponType = {"BasicBulletType","BasicBulletType","LaserBulletType","LightningBulletType", "MissileBulletType"};
 inline int chsAr{};
 inline std::string weaponName{};
 inline std::string shootSound{};
 inline std::string bulletType{};
 inline float reload{}, dmg{},inaccurate{}, weaponRotate{}, recoil{}, recoilTime{},recoilPow{}, cooldown{};
 inline float shootCone{},rotationLimit{};
-inline bool request = 1;
+inline bool request = 1; //config weapon base as activate
 inline int chWeapon{};
 int weaponSetup();
 int requestWeapon();
